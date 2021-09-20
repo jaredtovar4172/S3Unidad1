31c6ed609382e5ab002e01c02e4d640e1c497781
jaredtovar4172 <80796166+jaredtovar4172@users.noreply.github.com>
2021-09-20 09:46:51 -0500

    Add files via upload

---
+++ ListaAlumno/ListaAlumno/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListaAlumno
+{
+    public partial class Form1 : Form
+    {
+        Lista miLista;
+        ListaC listas;
+        public Form1()
+        {
+            InitializeComponent();
+            miLista = new Lista();
+            listas = new ListaC();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int numero = int.Parse(txtNumero.Text);
+                string matricula = txtMatricula.Text;
+                string nombre = txtNombre.Text;
+                string apellidoP = txtApellidoP.Text;
+                string apellidoM = txtApellidoM.Text;
+                string carrera = txtCarrera.Text;
+                Nodo n = new Nodo(numero, matricula, nombre, apellidoP, apellidoM, carrera, null);
+                miLista.Agregar(n);
+                txtMatricula.Clear();
+                txtNombre.Clear();
+                txtApellidoP.Clear();
+                txtApellidoM.Clear();
+                txtCarrera.Clear();
+                txtNumero.Focus();
+                miLista.Mostrar(lstLista);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error " + ex.Message);
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int numero = int.Parse(txtNumero.Text);
+                miLista.Eliminar(numero);
+                listas.EliminarC(numero);
+                txtMatricula.Clear();
+                txtNombre.Clear();
+                txtApellidoP.Clear();
+                txtApellidoM.Clear();
+                txtCarrera.Clear();
+                txtNumero.Focus();
+                miLista.Mostrar(lstLista);
+                listas.MostrarC(lsvLista);
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error " + ex.Message);
+            }
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int numero = int.Parse(txtNumero.Text);
+                Nodo b = new Nodo();
+                if (miLista.Buscar(numero, ref b))
+                {
+                    txtMatricula.Text = b.Matricula;
+                    txtNombre.Text = b.Nombre;
+                    txtApellidoP.Text = b.ApellidoP;
+                    txtApellidoM.Text = b.ApellidoM;
+                    txtCarrera.Text = b.Carrera;
+                }
+                else
+                {
+                    txtNumero.Clear();
+                    txtMatricula.Clear();
+                    txtNombre.Clear();
+                    txtApellidoP.Clear();
+                    txtApellidoM.Clear();
+                    txtCarrera.Clear();
+                    MessageBox.Show("NO Existe ");
+                }
+                txtNumero.Focus();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error " + ex.Message);
+            }
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int numero = int.Parse(txtNumero.Text);
+                miLista.Modificar(numero, txtMatricula.Text, txtNombre.Text, txtApellidoP.Text, txtApellidoM.Text, txtCarrera.Text);
+                txtNumero.Clear();
+                txtMatricula.Clear();
+                txtNombre.Clear();
+                txtApellidoP.Clear();
+                txtApellidoM.Clear();
+                txtCarrera.Clear();
+                txtNumero.Focus();
+                miLista.Mostrar(lstLista);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error " + ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+            int numero = int.Parse(txtNumero.Text);
+            string materia = txtMateria.Text;
+            string calificacion = txtCalificacion.Text;
+            NodoC n = new NodoC(numero, materia, calificacion);
+            listas.AgregarC(n);
+            txtNumero.Focus();
+            txtMateria.Clear();
+            txtCalificacion.Clear();
+            listas.MostrarC(lsvLista);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error " + ex.Message);
+            }
+        }
+    }
+}
---
+++ ListaAlumno/ListaAlumno/NodoC.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaAlumno
+{
+    class NodoC
+    {
+        private int numero;
+        private string materia;
+        private string calificacion;
+        private NodoC siguienteCal;
+        public int Numero
+        {
+            get { return numero; }
+            set { numero = value; }
+        }
+        public string Materia
+        {
+            get { return materia; }
+            set { materia = value; }
+        }
+        public string Calificacion
+        {
+            get { return calificacion; }
+            set { calificacion = value; }
+        }
+        public NodoC SiguienteCal
+        {
+            get { return siguienteCal; }
+            set { siguienteCal = value; }
+        }
+        public NodoC()
+        {
+            numero = 0;
+            materia = "";
+            calificacion = "";
+            siguienteCal = null;
+        }
+        public NodoC(int num, string m, string c)
+        {
+            numero = num;
+            materia = m;
+            calificacion = c;
+            siguienteCal = null;
+        }
+        public override string ToString()
+        {
+            return numero + materia + calificacion;
+        }
+    }
+}
